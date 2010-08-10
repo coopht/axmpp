61bcdf6b68512d5307d8a9c7b4c7ebc48acc86b8
alexander.basov <alexander.basov@f52679b2-8f1f-45d5-93a6-61a8106203f6>
2010-08-10 19:06:39 +0000

    Added definition of Stream Object

---
+++ source/xmpp-streams.adb
@@ -0,0 +1,64 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                                 AXMPP                                    --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2010 Alexander Basov <coopht@gmail.com>                      --
+--                                                                          --
+-- This is free software;  you can  redistribute it and/or modify it under  --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. UIM is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with UIM;  see file COPYING.  If not, write --
+-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
+-- Boston, MA 02110-1301, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+--
+--  <Unit> XMPP.Streams
+--  <ImplementationNotes>
+--
+------------------------------------------------------------------------------
+--  $Revision$ $Author$
+--  $Date$
+------------------------------------------------------------------------------
+with XMPP.Objects;
+with League.Strings;
+
+package body XMPP.Streams is
+
+   -----------------
+   --  Serialize  --
+   -----------------
+   function Serialize (Self : in XMPP_Stream)
+      return League.Strings.Universal_String
+   is
+   begin
+      raise Program_Error with "Not yet implemented";
+      return League.Strings.To_Universal_String ("");
+   end Serialize;
+
+   -------------------
+   --  Set_Content  --
+   -------------------
+   overriding
+   procedure Set_Content (Self      : in out XMPP_Stream;
+                          Parameter : League.Strings.Universal_String;
+                          Value     : League.Strings.Universal_String)
+   is
+   begin
+      raise Program_Error with "Not yet implemented";
+   end Set_Content;
+
+end XMPP.Streams;
