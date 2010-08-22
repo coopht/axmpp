9516082b6539ee5a814136d710bed37bba3ba96a
alexander.basov <alexander.basov@f52679b2-8f1f-45d5-93a6-61a8106203f6>
2010-08-22 17:07:27 +0000

    Added Connected handler

--- source/xmpp-stream_handlers.ads
+++ source/xmpp-stream_handlers.ads
@@ -7,6 +7,12 @@ package XMPP.Stream_Handlers is
 
    type XMPP_Stream_Handler_Access is access all XMPP_Stream_Handler'Class;
 
+   not overriding procedure Connected
+     (Self   : in out XMPP_Stream_Handler;
+      Object : XMPP.Stream_Features.XMPP_Stream_Feature_Access)
+      is null;
+   --  Connected handler is called after succesfull authentification
+
    not overriding procedure Start_Stream
      (Self   : in out XMPP_Stream_Handler;
       Object : not null XMPP.Streams.XMPP_Stream_Access) is null;
