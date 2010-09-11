12f7e6493ac2eb39d72102af4e6414b41e4d24a7
alexander.basov <alexander.basov@f52679b2-8f1f-45d5-93a6-61a8106203f6>
2010-09-11 15:40:30 +0000

    Added roster  skeleton

---
+++ source/xmpp-rosters.adb
@@ -0,0 +1,69 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                                 AXMPP                                    --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2010 Alexander Basov <coopht@gmail.com>                      --
+--                                                                          --
+-- This is free software;  you can  redistribute it and/or modify it under  --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. UIM is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with UIM;  see file COPYING.  If not, write --
+-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
+-- Boston, MA 02110-1301, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+--
+--  <Unit> XMPP.Sessions
+--  <ImplementationNotes>
+--
+------------------------------------------------------------------------------
+--  $Revision$ $Author$
+--  $Date$
+------------------------------------------------------------------------------
+package body XMPP.Rosters is
+
+   ----------------
+   --  Get_Kind  --
+   ----------------
+   overriding function Get_Kind (Self : XMPP_Roster)
+      return Objects.Object_Kind
+   is
+   begin
+      return XMPP.Objects.Roster;
+   end Get_Kind;
+
+   -----------------
+   --  Serialize  --
+   -----------------
+   overriding function Serialize (Self : in XMPP_Roster)
+      return League.Strings.Universal_String
+   is
+   begin
+      return X : League.Strings.Universal_String;
+   end Serialize;
+
+   -------------------
+   --  Set_Content  --
+   -------------------
+   overriding
+   procedure Set_Content (Self      : in out XMPP_Roster;
+                          Parameter : League.Strings.Universal_String;
+                          Value     : League.Strings.Universal_String) is
+   begin
+      raise Program_Error with "Not yet implemented";
+   end Set_Content;
+
+end XMPP.Rosters;
---
+++ source/xmpp-rosters.ads
@@ -0,0 +1,62 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                                 AXMPP                                    --
+--                                                                          --
+------------------------------------------------------------------------------
+--                                                                          --
+-- Copyright © 2010 Alexander Basov <coopht@gmail.com>                      --
+--                                                                          --
+-- This is free software;  you can  redistribute it and/or modify it under  --
+-- terms of the  GNU General Public License as published  by the Free Soft- --
+-- ware  Foundation;  either version 2,  or (at your option) any later ver- --
+-- sion. UIM is distributed in the hope that it will be useful, but WITH- --
+-- OUT ANY WARRANTY;  without even the  implied warranty of MERCHANTABILITY --
+-- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License --
+-- for  more details.  You should have  received  a copy of the GNU General --
+-- Public License distributed with UIM;  see file COPYING.  If not, write --
+-- to  the  Free Software Foundation,  51  Franklin  Street,  Fifth  Floor, --
+-- Boston, MA 02110-1301, USA.                                              --
+--                                                                          --
+-- As a special exception,  if other files  instantiate  generics from this --
+-- unit, or you link  this unit with other files  to produce an executable, --
+-- this  unit  does not  by itself cause  the resulting  executable  to  be --
+-- covered  by the  GNU  General  Public  License.  This exception does not --
+-- however invalidate  any other reasons why  the executable file  might be --
+-- covered by the  GNU Public License.                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+--
+--  <Unit> XMPP.Sessions
+--  <ImplementationNotes>
+--
+------------------------------------------------------------------------------
+--  $Revision$ $Author$
+--  $Date$
+------------------------------------------------------------------------------
+with League.Strings;
+
+with XMPP.Objects;
+
+package XMPP.Rosters is
+
+   type XMPP_Roster is new XMPP.Objects.XMPP_Object with private;
+
+   overriding function Get_Kind (Self : XMPP_Roster)
+      return Objects.Object_Kind;
+
+   overriding function Serialize (Self : in XMPP_Roster)
+      return League.Strings.Universal_String;
+
+   overriding
+   procedure Set_Content (Self      : in out XMPP_Roster;
+                          Parameter : League.Strings.Universal_String;
+                          Value     : League.Strings.Universal_String);
+
+private
+
+   type XMPP_Roster is new XMPP.Objects.XMPP_Object with
+   record
+      null;
+   end record;
+
+end XMPP.Rosters;
